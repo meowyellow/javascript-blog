67bdf4c1fc5f871a76b587e5250aa4cda508b2d0
Jakub Przydatek <xchubax@wp.pl>
2021-06-27 11:02:43 +0200

    Debugging

--- js/script.js
+++ js/script.js
@@ -38,3 +38,47 @@ const links = document.querySelectorAll('.titles a');
 for (let link of links) {
   link.addEventListener('click', titleClickHandler);
 }
+
+const optArticleSelector = '.post',
+  optTitleSelector = '.post-title',
+  optTitleListSelector = '.titles';
+
+function generateTitleLinks(customSelector = '') {
+
+  /* remove contents of titleList */
+  const titleList = document.querySelector(optTitleListSelector);
+  titleList.innerHTML = '';
+  /* for each article */
+  let html = '';
+  const articles = document.querySelectorAll(optArticleSelector + customSelector);
+  for (let article of articles) {
+
+    /* get the article id */
+    const articleId = article.getAttribute('id');
+    console.log(articleId);
+    /* find the title element */
+    const articleTitle = article.querySelector(optTitleSelector).innerHTML;
+    console.log(articleTitle);
+    /* get the title from the title element */
+
+    /* create HTML of the link */
+    const linkHTML = '<li><a href="#' + articleId + '"><span>' + articleTitle + '</span></a></li>';
+    console.log(linkHTML);
+
+    /* insert link into titleList */
+    html = html + linkHTML;
+
+
+    titleList.innerHTML = html;
+    const links = document.querySelectorAll('.titles a');
+    console.log('links', links);
+
+    for (let link of links) {
+      link.addEventListener('click', titleClickHandler);
+    }
+  }
+}
+
+
+
+generateTitleLinks();
